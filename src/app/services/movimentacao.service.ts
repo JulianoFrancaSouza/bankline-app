9c4c805db74c811e7f32eef3b1d6c5ff77e5e48b
Juliano França Souza <202111041529@alunos.estacio.br>
2022-05-26 09:42:19 -0300

    Update movimentacao.service.ts

--- src/app/services/movimentacao.service.ts
+++ src/app/services/movimentacao.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const baseUrl = 'http://localhost:8080';
+//const baseUrl = 'http://localhost:8080';
+const baseUrl = 'https://banklineapidio-project.herokuapp.com/';
 
 @Injectable({
   providedIn: 'root'
